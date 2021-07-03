77f5543d6c8ce1045bc0917fff7df23a231a2c80
Matthew Bouc <boucgm@gmail.com>
2021-07-02 20:05:54 -0500

    server side post

--- server/routes/taskListRouter.js
+++ server/routes/taskListRouter.js
@@ -20,17 +20,33 @@ pool.on('error', error => {
 
 
 taskRouter.get('/', (req, res) => {
-    const queryText ='SELECT * FROM to_do_list;';
-}).then().catch();
+    const queryText ='SELECT * FROM todo_list;';
+    pool.query(queryText).then(dbResponse => {
+        res.send(dbResponse.rows);
+    }).catch(error => {
+        console.log(`error retrieving from Database`, error);
+        res.sendStatus(500);
+    });
+});
 
 
-taskRouter.post().then().catch();
+taskRouter.post('/', (req, res) => {
+    const newTask = req.body
+    const values = [newTask.task, newTask.completed]
+    const queryText = `INSERT INTO todo_list (task, completed) VALUES ($1, $2);`;
+    pool.query(queryText, values).then(dbResponse => {
+        console.log(`POST successful`, dbResponse);
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log(`error posting to DB`, error);
+    });
+})
 
 
-taskRouter.delete().then().catch();
+taskRouter.delete()
 
 
-taskRouter.put().then().catch();
+taskRouter.put()
 
 
 
