3b6d4e7c35c6c35cf70c3be87628e3be420b8c12
Matthew Bouc <boucgm@gmail.com>
2021-07-02 19:19:03 -0500

    taskListGet() added

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,40 @@ $(document).ready(onReady);
 function onReady(){
     console.log('jq');
 }
+
+
+
+
+function taskListGET(){
+    $.ajax({
+        method: 'GET',
+        url: '/taskList'
+    })
+    .then(res => {
+        console.log('List retrieved from db', res);
+        // **********ADD FUNCTION TO APPEND TO DOM**************
+    })
+    .catch(err => {
+        console.log('ERROR caught on client side', err);
+    }
+        )
+
+}
+
+
+function taskListPOST(){
+
+
+}
+
+
+function taskListDELETE(){
+
+
+}
+
+
+function taskListPUT(){
+
+    
+}
